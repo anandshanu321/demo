687b2bdb36186e6c911fba84fc03cd2e853792ea
Shanu Anand <shanuanand411@gmail.com>
2025-10-04 16:49:18 +0530

    latest

--- src/test/java/Package/One.java
+++ src/test/java/Package/One.java
@@ -3,7 +3,10 @@ package Package;
 public class One {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("shanu");
+	System.out.println("shanu");
+	System.out.println("shanu");
+	System.out.println("shanu");
 
 	}
 
